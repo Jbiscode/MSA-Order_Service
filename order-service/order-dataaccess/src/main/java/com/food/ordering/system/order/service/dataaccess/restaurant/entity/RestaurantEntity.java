65e90fa207e58b30ad3dfc8b398a18183c19c4fc
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-07-29 15:43:13 +0900

    FEAT : order-dataaccess :: RESTAURANT JPA 레포지토리 & 어댑터 구현"

---
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/restaurant/entity/RestaurantEntity.java
@@ -0,0 +1,54 @@
+package com.food.ordering.system.order.service.dataaccess.restaurant.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.UUID;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@IdClass(RestaurantEntityId.class)
+@Table(name = "order_resataurant_m_view", schema = "restaurant")
+@Entity
+public class RestaurantEntity {
+
+    @Id
+    private UUID restaurantId;
+    @Id
+    private UUID productId;
+
+    private String restaurantName;
+    private Boolean restaurantActive;
+    private String productName;
+    private BigDecimal productPrice;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        RestaurantEntity that = (RestaurantEntity) o;
+        return getRestaurantId() != null && Objects.equals(getRestaurantId(), that.getRestaurantId())
+                && getProductId() != null && Objects.equals(getProductId(), that.getProductId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hash(restaurantId, productId);
+    }
+}
