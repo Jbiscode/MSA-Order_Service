e5ebb4e5dcc53d447c36dc141c1b3b40671db3aa
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-08-08 19:10:12 +0900

    FEAT : restaurant-service => restaurant-dataaccess :: dataaccess모듈 구현하기 & order-service에서도 사용하는 RestaurantEntity를 common-dataaccess로 이동해서 재사용 - OrderServiceApplication.java (basePackages 수정)

--- restaurant-service/restaurant-dataaccess/src/main/java/com/food/ordering/system/restaurant/service/dataaccess/restaurant/adapter/RestaurantRepositoryImpl.java
+++ restaurant-service/restaurant-dataaccess/src/main/java/com/food/ordering/system/restaurant/service/dataaccess/restaurant/adapter/RestaurantRepositoryImpl.java
@@ -1,19 +1,19 @@
-package com.food.ordering.system.order.service.dataaccess.restaurant.adapter;
+package com.food.ordering.system.restaurant.service.dataaccess.restaurant.adapter;
 
 import com.food.ordering.system.dataaccess.restaurant.entity.RestaurantEntity;
 import com.food.ordering.system.dataaccess.restaurant.repository.RestaurantJpaRepository;
-import com.food.ordering.system.order.service.dataaccess.restaurant.mapper.RestaurantDataAccessMapper;
-import com.food.ordering.system.order.service.domain.entity.Restaurant;
-import com.food.ordering.system.order.service.domain.ports.output.repository.RestaurantRepository;
+import com.food.ordering.system.restaurant.service.dataaccess.restaurant.mapper.RestaurantDataAccessMapper;
+import com.food.ordering.system.restaurant.service.domain.entity.Restaurant;
+import com.food.ordering.system.restaurant.service.domain.ports.output.repository.RestaurantRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@Repository
 @RequiredArgsConstructor
+@Component
 public class RestaurantRepositoryImpl implements RestaurantRepository {
 
     private final RestaurantJpaRepository restaurantJpaRepository;
@@ -23,12 +23,9 @@ public class RestaurantRepositoryImpl implements RestaurantRepository {
     public Optional<Restaurant> findRestaurantInformation(Restaurant restaurant) {
         List<UUID> restaurantProducts =
                 restaurantDataAccessMapper.restaurantToRestaurantProducts(restaurant);
-
-        Optional<List<RestaurantEntity>> restaurantEntities =
-                restaurantJpaRepository.findByRestaurantIdAndProductIdIn(
-                        restaurant.getId().getValue(), restaurantProducts);
-
-        return restaurantEntities
-                .map(restaurantDataAccessMapper::restaurantEntityToRestaurant);
+        Optional<List<RestaurantEntity>> restaurantEntities = restaurantJpaRepository
+                .findByRestaurantIdAndProductIdIn(restaurant.getId().getValue(),
+                        restaurantProducts);
+        return restaurantEntities.map(restaurantDataAccessMapper::restaurantEntityToRestaurant);
     }
 }
