04d5bcc3f56bcf3b9ac4c50aab0b8f36db355e57
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-08-09 11:13:28 +0900

    FEAT : restaurant-service => restaurant-messaging :: listener,publisher 구현하기

---
+++ restaurant-service/restaurant-messaging/src/main/java/com/food/ordering/system/restaurant/service/messaging/listener/kafka/RestaurantApprovalRequestKafkaListener.java
@@ -0,0 +1,47 @@
+package com.food.ordering.system.restaurant.service.messaging.listener.kafka;
+
+import com.food.ordering.system.kafka.consumer.KafkaConsumer;
+import com.food.ordering.system.kafka.order.avro.model.RestaurantApprovalRequestAvroModel;
+import com.food.ordering.system.restaurant.service.domain.ports.input.message.listener.RestaurantApprovalRequestMessageListener;
+import com.food.ordering.system.restaurant.service.messaging.mapper.RestaurantMessagingDataMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class RestaurantApprovalRequestKafkaListener implements KafkaConsumer<RestaurantApprovalRequestAvroModel> {
+
+    private final RestaurantApprovalRequestMessageListener restaurantApprovalRequestMessageListener;
+    private final RestaurantMessagingDataMapper restaurantMessagingDataMapper;
+
+    @Override
+    @KafkaListener(id = "${kafka-consumer-config.restaurant-approval-consumer-group-id}",
+            topics = "${restaurant-service.restaurant-approval-request-topic-name}")
+    public void receive(@Payload List<RestaurantApprovalRequestAvroModel> messages,
+                        @Header(KafkaHeaders.RECEIVED_KEY) List<String> keys,
+                        @Header(KafkaHeaders.RECEIVED_PARTITION) List<Integer> partitions,
+                        @Header(KafkaHeaders.OFFSET) List<Long> offsets) {
+        log.info("{}개의 메시지수신 ... messages='{}' key='{}' partitions='{}' and offset='{}'",
+                messages.size()
+                , messages
+                , keys.toString()
+                , partitions.toString()
+                , offsets.toString());
+
+        messages.forEach(restaurantApprovalRequestAvroModel -> {
+            log.info("식당승인요청을 처리중입니다. order id: {}", restaurantApprovalRequestAvroModel.getOrderId());
+            restaurantApprovalRequestMessageListener.approveOrder(
+                    restaurantMessagingDataMapper.restaurantApprovalRequestAvroModelToRestaurantApproval(restaurantApprovalRequestAvroModel)
+            );
+        });
+    }
+}
+
