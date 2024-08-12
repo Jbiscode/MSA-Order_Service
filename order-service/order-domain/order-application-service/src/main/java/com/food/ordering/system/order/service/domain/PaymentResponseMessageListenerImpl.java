9c7a9c215c7c4984300f5b1d0341359edf41188d
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-08-12 13:49:58 +0900

    FEAT : order-service => order-domain - PaymentResponseMessageListenerImpl.java 구현 완료

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/PaymentResponseMessageListenerImpl.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/PaymentResponseMessageListenerImpl.java
@@ -1,22 +1,35 @@
 package com.food.ordering.system.order.service.domain;
 
 import com.food.ordering.system.order.service.domain.dto.message.PaymentResponse;
+import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
 import com.food.ordering.system.order.service.domain.ports.input.message.listener.payment.PaymentResponseMessageListener;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
+import static com.food.ordering.system.order.service.domain.entity.Order.FAILURE_MESSAGE_DELIMITER;
+
 @Slf4j
+@RequiredArgsConstructor
 @Validated
 @Service
 public class PaymentResponseMessageListenerImpl implements PaymentResponseMessageListener {
+
+    private final OrderPaymentSaga orderPaymentSaga;
+
     @Override
     public void paymentCompleted(PaymentResponse paymentResponse) {
-
+        OrderPaidEvent domainEvent = orderPaymentSaga.process(paymentResponse);
+        log.info("OrderPaidEvent 발행중 {}", paymentResponse.getOrderId());
+        domainEvent.fire();
     }
 
     @Override
     public void paymentCancelled(PaymentResponse paymentResponse) {
-
+        orderPaymentSaga.rollback(paymentResponse);
+        log.info("주문결제 롤백 처리중 :{}  실패사유: {}",
+                paymentResponse.getOrderId(),
+                String.join(FAILURE_MESSAGE_DELIMITER, paymentResponse.getFailureMessages()));
     }
 }
