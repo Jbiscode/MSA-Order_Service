3b733abc75823a19bb290ba5d323417ee0712123
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-08-16 18:24:09 +0900

    REFACTOR : order-service :: order-messaging KafkaListener 수정

--- order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/listener/kafka/PaymentResponseKafkaListener.java
+++ order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/listener/kafka/PaymentResponseKafkaListener.java
@@ -3,10 +3,12 @@ package com.food.ordering.system.order.service.messaging.listener.kafka;
 import com.food.ordering.system.kafka.consumer.KafkaConsumer;
 import com.food.ordering.system.kafka.order.avro.model.PaymentResponseAvroModel;
 import com.food.ordering.system.kafka.order.avro.model.PaymentStatus;
+import com.food.ordering.system.order.service.domain.exception.OrderNotFoundException;
 import com.food.ordering.system.order.service.domain.ports.input.message.listener.payment.PaymentResponseMessageListener;
 import com.food.ordering.system.order.service.messaging.mapper.OrderMessagingDataMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
@@ -52,8 +54,13 @@ public class PaymentResponseKafkaListener implements KafkaConsumer<PaymentRespon
                             orderMessagingDataMapper.paymentResponseAvroModelToPaymentResponseEvent(paymentResponseAvroModel)
                     );
                 }
-            } catch (Exception e) {
-                log.error("주문결제응답을 처리하는 도중 오류가 발생했습니다. paymentResponseAvroModel: {} & error: {}",
+            } catch (OptimisticLockingFailureException e) {
+                // 낙관적잠금은 예외처리가 필요하지않다. 다른 스레드가 작업을 완료했으니 끝내면 됨.
+                log.error("주문결제응답을 처리하는 도중 오류가 발생했습니다. 낙관적잠금. paymentResponseAvroModel: {} & error: {}",
+                        paymentResponseAvroModel, e.getMessage());
+            } catch (OrderNotFoundException e){
+                // 이 예외도 그냥 끝내면 됨. 주문을 찾을 수 없음. 예외를 잡아줘서 재시도를 막아줘야함.
+                log.error("주문결제응답을 처리하는 도중 오류가 발생했습니다. 주문을 찾을 수 없음. paymentResponseAvroModel: {} & error: {}",
                         paymentResponseAvroModel, e.getMessage());
             }
         });
--- order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/listener/kafka/RestaurantApprovalResponseKafkaListener.java
+++ order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/listener/kafka/RestaurantApprovalResponseKafkaListener.java
@@ -3,10 +3,12 @@ package com.food.ordering.system.order.service.messaging.listener.kafka;
 import com.food.ordering.system.kafka.consumer.KafkaConsumer;
 import com.food.ordering.system.kafka.order.avro.model.OrderApprovalStatus;
 import com.food.ordering.system.kafka.order.avro.model.RestaurantApprovalResponseAvroModel;
+import com.food.ordering.system.order.service.domain.exception.OrderNotFoundException;
 import com.food.ordering.system.order.service.domain.ports.input.message.listener.restaurantapproval.RestaurantApprovalResponseMessageListener;
 import com.food.ordering.system.order.service.messaging.mapper.OrderMessagingDataMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
@@ -41,6 +43,7 @@ public class RestaurantApprovalResponseKafkaListener implements KafkaConsumer<Re
 
         messages.forEach(restaurantApprovalResponseAvroModel -> {
 
+            try {
                 if (OrderApprovalStatus.APPROVED == restaurantApprovalResponseAvroModel.getOrderApprovalStatus()) {
                     log.info("식당승인응답을 처리합니다. order id: {}", restaurantApprovalResponseAvroModel.getOrderId());
                     restaurantApprovalResponseMessageListener.orderApproved(
@@ -53,6 +56,15 @@ public class RestaurantApprovalResponseKafkaListener implements KafkaConsumer<Re
                             orderMessagingDataMapper.restaurantApprovalResponseAvroModelToRestaurantApprovalResponseEvent(restaurantApprovalResponseAvroModel)
                     );
                 }
+            } catch (OptimisticLockingFailureException e) {
+                // 낙관적잠금은 예외처리가 필요하지않다. 다른 스레드가 작업을 완료했으니 끝내면 됨.
+                log.error("주문결제응답을 처리하는 도중 오류가 발생했습니다. 낙관적잠금. restaurantApprovalResponseAvroModel: {} & error: {}",
+                        restaurantApprovalResponseAvroModel, e.getMessage());
+            } catch (OrderNotFoundException e){
+                // 이 예외도 그냥 끝내면 됨. 주문을 찾을 수 없음. 예외를 잡아줘서 재시도를 막아줘야함.
+                log.error("주문결제응답을 처리하는 도중 오류가 발생했습니다. 주문을 찾을 수 없음. restaurantApprovalResponseAvroModel: {} & error: {}",
+                        restaurantApprovalResponseAvroModel, e.getMessage());
+            }
 
         });
     }
