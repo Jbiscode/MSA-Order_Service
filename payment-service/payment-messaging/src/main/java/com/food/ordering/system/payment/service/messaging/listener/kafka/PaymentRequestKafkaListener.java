eb0e12cc7fcb2c2120c5803e08b5d400641cfcda
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-08-20 13:49:07 +0900

    FEAT : payment-service :: payment-messaging 리스너 예외처리

--- payment-service/payment-messaging/src/main/java/com/food/ordering/system/payment/service/messaging/listener/kafka/PaymentRequestKafkaListener.java
+++ payment-service/payment-messaging/src/main/java/com/food/ordering/system/payment/service/messaging/listener/kafka/PaymentRequestKafkaListener.java
@@ -3,16 +3,21 @@ package com.food.ordering.system.payment.service.messaging.listener.kafka;
 import com.food.ordering.system.kafka.consumer.KafkaConsumer;
 import com.food.ordering.system.kafka.order.avro.model.PaymentOrderStatus;
 import com.food.ordering.system.kafka.order.avro.model.PaymentRequestAvroModel;
+import com.food.ordering.system.payment.service.domain.exception.PaymentApplicationServiceException;
+import com.food.ordering.system.payment.service.domain.exception.PaymentNotFoundException;
 import com.food.ordering.system.payment.service.domain.ports.input.message.listener.PaymentRequestMessageListener;
 import com.food.ordering.system.payment.service.messaging.mapper.PaymentMessagingDataMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.postgresql.util.PSQLState;
+import org.springframework.dao.DataAccessException;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import java.sql.SQLException;
 import java.util.List;
 
 @Slf4j
@@ -38,16 +43,31 @@ log.info("{}개의 메시지수신 ... messages='{}' key='{}' partitions='{}' an
                 , offsets.toString());
 
         messages.forEach(paymentRequestAvroModel -> {
-            if(paymentRequestAvroModel.getPaymentOrderStatus() == PaymentOrderStatus.PENDING) {
-                log.info("주문결제요청을 처리중입니다. order id: {}", paymentRequestAvroModel.getOrderId());
-                paymentRequestMessageListener.completePayment(
-                        paymentMessagingDataMapper.paymentRequestAvroModelToPaymentRequest(paymentRequestAvroModel)
-                );
-            }else if (paymentRequestAvroModel.getPaymentOrderStatus() == PaymentOrderStatus.CANCELLED) {
-                log.info("주문결제요청을 취소합니다. order id: {}", paymentRequestAvroModel.getOrderId());
-                paymentRequestMessageListener.cancelPayment(
-                        paymentMessagingDataMapper.paymentRequestAvroModelToPaymentRequest(paymentRequestAvroModel)
-                );
+            try {
+                if(paymentRequestAvroModel.getPaymentOrderStatus() == PaymentOrderStatus.PENDING) {
+                    log.info("주문결제요청을 처리중입니다. order id: {}", paymentRequestAvroModel.getOrderId());
+                    paymentRequestMessageListener.completePayment(
+                            paymentMessagingDataMapper.paymentRequestAvroModelToPaymentRequest(paymentRequestAvroModel)
+                    );
+                }else if (paymentRequestAvroModel.getPaymentOrderStatus() == PaymentOrderStatus.CANCELLED) {
+                    log.info("주문결제요청을 취소합니다. order id: {}", paymentRequestAvroModel.getOrderId());
+                    paymentRequestMessageListener.cancelPayment(
+                            paymentMessagingDataMapper.paymentRequestAvroModelToPaymentRequest(paymentRequestAvroModel)
+                    );
+                }
+            } catch (DataAccessException e) {
+                SQLException sqlException = (SQLException) e.getRootCause();
+                if (sqlException!=null
+                        && sqlException.getSQLState() != null
+                        && PSQLState.UNIQUE_VIOLATION.getState().equals(sqlException.getSQLState())) {
+                    log.error("주문결제요청이 중복되었습니다.UNIQUE_VIOLATION:{} order id: {}",
+                            sqlException.getSQLState(),
+                            paymentRequestAvroModel.getOrderId());
+                }else {
+                    throw new PaymentApplicationServiceException("주문결제요청 처리중 예외에러가 발생했습니다." + e.getMessage(),e);
+                }
+            }catch (PaymentNotFoundException e) {
+                log.error("주문결제요청을 찾지 못했습니다. orderId: {}", paymentRequestAvroModel.getOrderId());
             }
         });
     }
--- payment-service/payment-messaging/src/main/java/com/food/ordering/system/payment/service/messaging/mapper/PaymentMessagingDataMapper.java
+++ payment-service/payment-messaging/src/main/java/com/food/ordering/system/payment/service/messaging/mapper/PaymentMessagingDataMapper.java
@@ -4,11 +4,7 @@ import com.food.ordering.system.kafka.order.avro.model.PaymentRequestAvroModel;
 import com.food.ordering.system.kafka.order.avro.model.PaymentResponseAvroModel;
 import com.food.ordering.system.kafka.order.avro.model.PaymentStatus;
 import com.food.ordering.system.payment.service.domain.dto.PaymentRequest;
-import com.food.ordering.system.payment.service.domain.entity.Payment;
-import com.food.ordering.system.payment.service.domain.event.PaymentCancelledEvent;
-import com.food.ordering.system.payment.service.domain.event.PaymentCompletedEvent;
-import com.food.ordering.system.payment.service.domain.event.PaymentEvent;
-import com.food.ordering.system.payment.service.domain.event.PaymentFailedEvent;
+import com.food.ordering.system.payment.service.domain.outbox.model.OrderEventPayload;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
@@ -17,17 +13,21 @@ import java.util.UUID;
 @Component
 public class PaymentMessagingDataMapper {
 
-    public PaymentResponseAvroModel paymentCompletedEventToPaymentResponseAvroModel(PaymentCompletedEvent paymentCompletedEvent) {
-        return getPaymentResponseAvroModel(paymentCompletedEvent);
+    public PaymentResponseAvroModel orderEventPayloadToPaymentResponseAvroModel(String SagaId,
+                                                                                OrderEventPayload orderEventPayload) {
+        return PaymentResponseAvroModel.newBuilder()
+                .setId(UUID.randomUUID())
+                .setSagaId(UUID.fromString(SagaId))
+                .setPaymentId(UUID.fromString(orderEventPayload.getPaymentId()))
+                .setCustomerId(UUID.fromString(orderEventPayload.getCustomerId()))
+                .setOrderId(UUID.fromString(orderEventPayload.getOrderId()))
+                .setPrice(orderEventPayload.getPrice())
+                .setCreatedAt(orderEventPayload.getCreatedAt().toInstant())
+                .setPaymentStatus(PaymentStatus.valueOf(orderEventPayload.getPaymentStatus()))
+                .setFailureMessages(orderEventPayload.getFailureMessages())
+                .build();
     }
 
-    public PaymentResponseAvroModel paymentCancelledEventToPaymentResponseAvroModel(PaymentCancelledEvent paymentCancelledEvent) {
-        return getPaymentResponseAvroModel(paymentCancelledEvent);
-    }
-
-    public PaymentResponseAvroModel paymentFailedEventToPaymentResponseAvroModel(PaymentFailedEvent paymentFailedEvent) {
-        return getPaymentResponseAvroModel(paymentFailedEvent);
-    }
 
     public PaymentRequest paymentRequestAvroModelToPaymentRequest(PaymentRequestAvroModel paymentRequestAvroModel) {
         return PaymentRequest.builder()
@@ -37,18 +37,4 @@ public class PaymentMessagingDataMapper {
                 .build();
     }
 
-    private PaymentResponseAvroModel getPaymentResponseAvroModel(PaymentEvent paymentCompletedEvent) {
-        Payment payment = paymentCompletedEvent.getPayment();
-        return PaymentResponseAvroModel.newBuilder()
-                .setId(UUID.randomUUID())
-                .setSagaId(UUID.randomUUID())
-                .setPaymentId(UUID.fromString(payment.getId().getValue().toString()))
-                .setCustomerId(UUID.fromString(payment.getCustomerId().getValue().toString()))
-                .setOrderId(UUID.fromString(payment.getOrderId().getValue().toString()))
-                .setPrice(payment.getPrice().getAmount())
-                .setCreatedAt(payment.getCreatedAt().toInstant())
-                .setPaymentStatus(PaymentStatus.valueOf(payment.getPaymentStatus().name()))
-                .setFailureMessages(paymentCompletedEvent.getFailureMessages())
-                .build();
-    }
 }
